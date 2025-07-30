2cea7dc06d467bea2aae2ef0ef2fe92f4f5d5195
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2025-07-30 18:32:19 -0300

    implementando novas funções no carrinho

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ function App() {
 
         setIsModalOpen(prev => !prev)
   }
+  const [allFoodData, setAllFoodData] = useState<Array<any>>([]);
+  const [total, setTotal] = useState(0);
+  const [countFoodData, setCountFoodData] = useState(0);
 
 
   return (
@@ -23,11 +26,15 @@ function App() {
            
            <>
                 <Card
-                id={foodData.id}
-                price={foodData.price}
-                title={foodData.title}
-                image={foodData.image} />
-
+                  price={foodData.price}
+                  title={foodData.title}
+                  image={foodData.image} id={0} allFoodData={[]} setAllFoodData={function (value: any[]): void {
+                    throw new Error('Function not implemented.');
+                  } } total={0} setTotal={function (value: number): void {
+                    throw new Error('Function not implemented.');
+                  } } countFoodData={0} setCountFoodData={function (value: number): void {
+                    throw new Error('Function not implemented.');
+                  } } />
                 </>
                 
            )}
@@ -35,6 +42,13 @@ function App() {
         {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
         <button onClick={handleOpenModal}>Novo</button>
       </div>
+      <Card 
+        allFoodData={allFoodData}
+        setAllFoodData={setAllFoodData}
+        total={total}
+        setTotal={setTotal}
+        countFoodData={countFoodData}
+        setCountFoodData={setCountFoodData} id={0} price={0} title={''} image={''}      />
     </>
   )
 }
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -6,14 +6,42 @@ interface CardProps{
     id: number,
     price: number,
     title: string,
-    image: string
+    image: string,
+    allFoodData: any[],
+    setAllFoodData(value: any[]): void,
+    total : number,
+    setTotal (value: number): void,
+    countFoodData: number,
+    setCountFoodData(value: number): void,
 
 
 }
 
-export function Card({id, price, title, image} : CardProps){
+
+
+export function Card({id, price, title, image, allFoodData, setAllFoodData, total, setTotal, countFoodData, setCountFoodData} : CardProps){
 
     const quantity = 1
+
+    const onAddFoodData = (foodData: { id: any; title?: string; price: any; quantity: any; }) => {
+
+
+        if(allFoodData.find(item  => item.id === foodData.id)){
+  
+           const listFoodData = allFoodData.map(item  => item.id === foodData.id ? {...item, quantity: item.quantity + 1} : item
+              );
+          setTotal(total + foodData.price * foodData.quantity);
+          setCountFoodData(countFoodData + foodData.quantity);
+          return setAllFoodData([...listFoodData]);
+  
+        }
+  
+        setTotal(total + foodData.price * foodData.quantity);
+        setCountFoodData(countFoodData + foodData.quantity);
+        setAllFoodData([...allFoodData, foodData]);
+  
+  };
+
 
     return(
        
@@ -27,25 +55,16 @@ export function Card({id, price, title, image} : CardProps){
 
     </div>
 
+    
+
     )
+
+    
 }
 
-/*const onAddFoodData = foodData => {
+//export function FoodDataList({allFoodData, setAllFoodData, total, setTotal,countFoodData, setCountFoodData}) : FoodDataList {
 
 
-      if(allFoodData.find(item => item.id === foodData.id)){
+//
 
-         const listFoodData = allFoodData.map(item => item.id === foodData.id ? {...item, quantity: item.quantity + 1} : item
-            );
-        setTotal(total + foodData.price * foodData.quantity);
-        setCountFoodData(countFoodData + foodData.quantity);
-        return setAllFoodData([...listFoodData]);
-
-      }
-
-      setTotal(total + foodData.price * foodData.quantity);
-      setCountFoodData(countFoodData + foodData.quantity);
-      setAllProducts([...allFoodData, foodData]);
-
-};*/
 
